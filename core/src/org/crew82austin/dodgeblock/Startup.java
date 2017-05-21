9ef46cef003ae7dc982abbd5cce768962d250db2
jrspotts <justinrspotts@gmail.com>
2017-05-21 15:35:18 -0500

    More Starter

--- core/src/org/crew82austin/dodgeblock/Startup.java
+++ core/src/org/crew82austin/dodgeblock/Startup.java
@@ -15,48 +15,60 @@ public class Startup implements ActionListener{
 
 	private boolean isHosting;
 	private JFrame frame;
-	private String userInput1;
+	
+	public String name1;
+	public String name2;
+	public String IP;
+	public String Port;
+	private boolean start;
+	
 	JTextField tField1;
 	
 	public Startup(){
 		frame = new JFrame("Setup");
-		userInput1 = "";
+		name1 = "";
+		name2 = "";
+		IP = "";
+		Port = "";
+		start = false;
 	}
 	
 	public void displayStart(){
 		System.out.println("Making Startup Window");
-		Button button = new Button();
-		button.setLabel("PRESS MEEE!!!!!!!");
-		button.setActionCommand("displayMessage");
+		
+		Button button = new Button("Host");
+		button.setActionCommand("getHostInfo");
 		button.addActionListener(this);
-		button.setSize(1920, 500);
 		
 	    tField1 = new JTextField(20);
 		tField1.setActionCommand("typed");
 		tField1.addActionListener(this);
 		
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.getContentPane().add(button, BorderLayout.SOUTH);
-		frame.getContentPane().add(tField1, BorderLayout.NORTH);
+		frame.getContentPane().add(button);
 		frame.pack();
 		
 		frame.setSize(1920, 1080);
 		frame.setVisible(true);
 	}
 	
-	public void displayMessage(){
-		System.out.println("Displaying Messsage");
-		JLabel label = new JLabel("You Typed: "+tField1.getText(), JLabel.CENTER); 
-		label.setFont(new Font("response", Font.PLAIN, 50));
-		frame.getContentPane().add(label, BorderLayout.CENTER);
-		frame.pack();
+	public void getHostInfo(){
+		System.out.println("Getting host Info");
+		frame.getContentPane().removeAll();;
+		Button okButton = new Button("OK");
+		okButton.setActionCommand("goToWaiting");
+		okButton.addActionListener(this);
 		
-		frame.setSize(1920, 1080);
+		frame.getContentPane().add(okButton);
+		frame.pack();
 	}
 	
 	public void actionPerformed(ActionEvent e){
-		if("displayMessage".equals(e.getActionCommand())){
-			displayMessage();
+		if("getHostInfo".equals(e.getActionCommand())){
+			getHostInfo();
+		}
+		if("goToWaiting".equals(e.getActionCommand())){
+			
 		}
 		
 	}
