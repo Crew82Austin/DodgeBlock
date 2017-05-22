e95e5a4efa23fd312b6ab9cb5e2ac9c2a25876c1
jrspotts <justinrspotts@gmail.com>
2017-05-22 13:55:59 -0500

    Initial Work on Entering Username

--- core/src/org/crew82austin/dodgeblock/MainMenu.java
+++ core/src/org/crew82austin/dodgeblock/MainMenu.java
@@ -1,14 +1,21 @@
 package org.crew82austin.dodgeblock;
 
+import java.awt.Button;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class MainMenu implements Screen{
@@ -18,11 +25,23 @@ public class MainMenu implements Screen{
 	private Stage stage;
 	private BitmapFont font;
 	private Game myGame;
-	
+	private TextField userName;
+	private Dialog errorBox;
+	private float timeSinceBox; //Time Since the Error Dialogue has appeared
+	private float errorTime; //How long the Error Dialogue should be visible
 	
 	public MainMenu(Game game){
 		batch = new SpriteBatch();
+		
 		font = new BitmapFont();
+		font.getData().scale(2f);
+		
+		Window.WindowStyle errorStyle = new Window.WindowStyle(font, Color.WHITE, null);
+		errorBox = new Dialog("You Dun Goofed", errorStyle);
+	
+		timeSinceBox = 0f;
+		errorTime = 2000f;
+		
 		myGame = game;
 	
 	}
@@ -34,22 +53,42 @@ public class MainMenu implements Screen{
 		stage.clear();
 		Gdx.input.setInputProcessor(stage);
 		
-		TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
 		
-		style.font = font;
 		
-		startBtn = new TextButton("Start!", style);
-		startBtn.setPosition(500f, 500f);
-		startBtn.setSize(200f, 40f);
-		startBtn.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                 myGame.setScreen(new GameScreen());
-            }
-        });
 		
-		stage.addActor(startBtn);
 		
+		//Add User Text Field
+		userName = new TextField("", new TextField.TextFieldStyle(
+				font, Color.BLACK, null, null, null));
+		userName.setMessageText("Username");
+		userName.setPosition(1920f/2, 40f);
+		userName.setSize(300f, 40f);
+		stage.addActor(userName);
+		
+		//Add Start Button
+				TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+				
+				style.font = font;
+				
+				startBtn = new TextButton("Start!", style);
+				startBtn.setPosition(1920f/2, 500f);
+				startBtn.setSize(200f, 40f);
+				startBtn.addListener(new ClickListener(){
+		            @Override
+		            public void clicked(InputEvent event, float x, float y) {
+		            	 if(userName.getText().isEmpty()){
+		            		
+		            		 userName.setMessageText("Enter Name Here");
+		            		 errorBox.show(stage);
+		            	 }
+		            	 else{
+		            		 myGame.setScreen(new GameScreen());
+		            	 }
+		            }
+		        });
+				
+				stage.addActor(startBtn);
+				
 	}
 
 	@Override
@@ -65,7 +104,20 @@ public class MainMenu implements Screen{
 		stage.draw();
 		batch.end();
 		
+		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
+			Gdx.app.exit();
+		}
 		
+		//Handling of Error Dialogue
+		if(errorBox.isVisible()){
+			errorTime += delta;
+			if(delta > errorTime){
+				errorBox.hide();
+			}
+		}
+		else{
+			errorTime = 0f;
+		}
 
 	}
 
