45a8f14e6b64f1d17473698a470c311c70931d8f
jrspotts <justinrspotts@gmail.com>
2017-05-29 16:36:10 -0500

    Updated Main Menu

--- core/src/org/crew82austin/dodgeblock/MainMenu.java
+++ core/src/org/crew82austin/dodgeblock/MainMenu.java
@@ -10,9 +10,11 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
@@ -22,6 +24,10 @@ public class MainMenu implements Screen{
 	
 	private SpriteBatch batch;
 	private TextButton hostBtn;
+	private TextButton joinBtn;
+	private TextButton optionsBtn;
+	private Texture logoTexture;
+	private Image logo;
 	private Stage stage;
 	private BitmapFont font;
 	private Game myGame;
@@ -29,7 +35,7 @@ public class MainMenu implements Screen{
 	private Dialog errorBox;
 	private float timeSinceBox; //Time Since the Error Dialogue has appeared
 	private float errorTime; //How long the Error Dialogue should be visible
-	private boolean displayingError;
+	private boolean displayingError;//Is that box being displayed?
 	
 	public MainMenu(Game game){
 		batch = new SpriteBatch();
@@ -43,6 +49,11 @@ public class MainMenu implements Screen{
 		timeSinceBox = 0f;
 		errorTime = 2f;
 		displayingError = false;
+		
+		logoTexture = new Texture(Gdx.files.internal("DB.png"));
+		logo = new Image(logoTexture);
+		logo.setPosition((Gdx.graphics.getWidth() / 2f) - 250f, 
+				Gdx.graphics.getHeight() - 400f);
 		
 		myGame = game;
 	
@@ -59,41 +70,91 @@ public class MainMenu implements Screen{
 		
 		
 		
-		//Add User Text Field
+		//Add Name Text Field
 		userName = new TextField("", new TextField.TextFieldStyle(
 				font, Color.BLACK, null, null, null));
-		userName.setMessageText("Username");
-		userName.setPosition((1920f/2)-150f, 40f);
-		userName.setSize(300f, 40f);
+		userName.setMessageText("         Username");//Spaces for Centering
+		userName.setPosition(
+				(Gdx.graphics.getWidth()/2) - 250f,
+				(Gdx.graphics.getHeight() - 500f));
+		userName.setSize(500f, 100f);
 		stage.addActor(userName);
 		
-		//Add Start Button
-				TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+		//Add Host Button
+		TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
 				
-				style.font = font;
+		style.font = font;
 				
-				hostBtn = new TextButton("Host", style);
-				hostBtn.setPosition(
-						(Gdx.graphics.getWidth() / 4) - 100f,
-						Gdx.graphics.getHeight() /2);
-				hostBtn.setSize(200f, 40f);
-				hostBtn.addListener(new ClickListener(){
-		            @Override
-		            public void clicked(InputEvent event, float x, float y) {
-		            	 if(userName.getText().isEmpty()){
+		hostBtn = new TextButton("Host", style);
+		hostBtn.setPosition(
+				(Gdx.graphics.getWidth()/2) + 175f,
+				(Gdx.graphics.getHeight()/3));
+		hostBtn.setSize(200f, 40f);
+		hostBtn.addListener(new ClickListener(){
+		        @Override
+		          public void clicked(InputEvent event, float x, float y) {
+		            if(userName.getText().isEmpty()){
 		            		
-		            		 userName.setMessageText("Enter Name Here");
-		            		 errorBox.show(stage);
-		            		 displayingError = true;
-		            	 }
-		            	 else{
-		            		 myGame.setScreen(new GameScreen());
-		            	 }
+		            	userName.setMessageText("         Enter Name Here");//Spacing for Centering
+		            	errorBox.show(stage);
+		            	displayingError = true;
 		            }
-		        });
+		            else{
+		            	myGame.setScreen(new GameScreen());
+		            }
+		          }
+		      });
 				
-				stage.addActor(hostBtn);
+		stage.addActor(hostBtn);
 				
+		//Add Join Button
+		joinBtn = new TextButton("Join", style);
+		joinBtn.setPosition(
+				(Gdx.graphics.getWidth()/2) - 300f,
+				(Gdx.graphics.getHeight()/3));
+		joinBtn.setSize(200f, 40f);
+		joinBtn.addListener( new ClickListener(){
+			@Override
+	          public void clicked(InputEvent event, float x, float y) {
+	            if(userName.getText().isEmpty()){
+	            		
+	            	userName.setMessageText("         Enter Name Here");//Spacing for Centering
+	            	errorBox.show(stage);
+	            	displayingError = true;
+	            }
+	            else{
+	            	myGame.setScreen(new GameScreen());
+	            }
+	          }
+		});
+		
+		stage.addActor(joinBtn);
+				
+		//Add Options Button 
+		optionsBtn = new TextButton("Options", style);
+		optionsBtn.setPosition(
+				(Gdx.graphics.getWidth() / 2) - 100f,
+				(0f));
+		optionsBtn.setSize(200f, 40f);
+		optionsBtn.addListener(new ClickListener(){
+			@Override
+	          public void clicked(InputEvent event, float x, float y) {
+	            if(userName.getText().isEmpty()){
+	            		
+	            	userName.setMessageText("   You Clicked Options");//Spacing for Centering
+	            }
+	            else{
+	            	myGame.setScreen(new GameScreen());
+	            }
+	          }
+		});
+		
+		stage.addActor(optionsBtn);
+		
+		
+		//Add Logo
+		stage.addActor(logo);
+		
 	}
 
 	@Override
